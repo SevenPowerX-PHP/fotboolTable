95a8da17601368c122deba514ef0e1588b130e85
splaa-Acer <splaandrey@gmail.com>
2018-12-28 07:23:03 +0100

    Default Changelist

--- README/traklist.md
+++ README/traklist.md
@@ -1,1 +1,9 @@
-- [ ] [Таблица Commands](https://www.videosharp.info/video/footpost1/nr=2125) - 00:05:50
+- [x] 5.[Таблица Commands](https://www.videosharp.info/video/footpost1/nr=2125)
+- [x] 6.[Таблица Players - игроки](https://www.videosharp.info/video/footpost1/nr=2126)
+- [ ] 7.[Остальные таблицы и замена на поле](https://www.videosharp.info/video/footpost1/nr=2127)
+- [ ] 8.[Подключение php к postgresql](https://www.videosharp.info/video/footpost1/nr=2128)
+- [ ] 9.[Команды - в массив ](https://www.videosharp.info/video/footpost1/nr=2129)
+- [ ] 10.[Представление имеем и ... используем ](https://www.videosharp.info/video/footpost1/nr=2143)
+- [ ] 11.[Добавляем всё! ](https://www.videosharp.info/video/footpost1/nr=2144)
+- [ ] 12.[Массивные игроки ](https://www.videosharp.info/video/footpost1/nr=2153)
+- [ ] 13.[Вы хорошо сохранились? ](https://www.videosharp.info/video/footpost1/nr=2154)
---
+++ data/INSERT_players_ru.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.players (id, id_command, player, play_number, info) VALUES (1, 4, 'Игорь Акинфеев', '1', 'Капитан команды.Вратарь');
+INSERT INTO public.players (id, id_command, player, play_number, info) VALUES (2, 4, 'Марио Фернандес', '2', 'Защитник');
+INSERT INTO public.players (id, id_command, player, play_number, info) VALUES (3, 4, 'Илья Кутепов', '3', 'Защитник');
+INSERT INTO public.players (id, id_command, player, play_number, info) VALUES (4, 4, 'Сергей Игнашевич', '4', 'Защитник');
+INSERT INTO public.players (id, id_command, player, play_number, info) VALUES (5, 4, 'Андрей Семёнов', '5', 'Защитник');
+INSERT INTO public.players (id, id_command, player, play_number, info) VALUES (6, 4, 'Денис Черышев', '6', 'Полузащитник');
+INSERT INTO public.players (id, id_command, player, play_number, info) VALUES (7, 4, 'Далер Кузяев', '7', 'Полузащитник');
+INSERT INTO public.players (id, id_command, player, play_number, info) VALUES (8, 4, 'Юрий Газинский', '8', 'Полузащитник');
+INSERT INTO public.players (id, id_command, player, play_number, info) VALUES (9, 4, 'Алан Дзагоев', '9', 'Полузащитник');
+INSERT INTO public.players (id, id_command, player, play_number, info) VALUES (10, 4, 'Фёдор Смолов', '10', 'Нападающий');
+INSERT INTO public.players (id, id_command, player, play_number, info) VALUES (11, 4, 'Роман Зобнин', '11', 'Полузащитник');
