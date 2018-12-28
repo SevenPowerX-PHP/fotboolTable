0365fb8783eeea8f1a3e1f96bcbc8b18fa27a04a
splaa-Acer <splaandrey@gmail.com>
2018-12-28 07:52:33 +0100

    Added sql file Create table Games

---
+++ data/CREATE_TABLE_Games.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Games(
+                    Id SERIAL NOT NULL PRIMARY KEY ,
+                    Game_data DATE,
+                    Status CHAR(7),
+                    Info TEXT
+);
