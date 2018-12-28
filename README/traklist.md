e8b9949b7fb7e75ade173165c9da20d663ce9687
splaa-Acer <splaandrey@gmail.com>
2018-12-28 15:48:20 +0100

    fotboolTable-9 Postres Представления

--- README/traklist.md
+++ README/traklist.md
@@ -3,7 +3,7 @@
 - [x] 7.[Остальные таблицы и замена на поле](https://www.videosharp.info/video/footpost1/nr=2127)
 - [x] 8.[Подключение php к postgresql](https://www.videosharp.info/video/footpost1/nr=2128)
 - [x] 9.[Команды - в массив ](https://www.videosharp.info/video/footpost1/nr=2129)
-- [ ] 10.[Представление имеем и ... используем ](https://www.videosharp.info/video/footpost1/nr=2143)
+- [x] 10.[Представление имеем и ... используем ](https://www.videosharp.info/video/footpost1/nr=2143)
 - [ ] 11.[Добавляем всё! ](https://www.videosharp.info/video/footpost1/nr=2144)
 - [ ] 12.[Массивные игроки ](https://www.videosharp.info/video/footpost1/nr=2153)
 - [ ] 13.[Вы хорошо сохранились? ](https://www.videosharp.info/video/footpost1/nr=2154)
--- web/index.php
+++ web/index.php
@@ -7,21 +7,42 @@
 	 */
 	echo 'football';
 	$conn = pg_connect('host=localhost port=5432 dbname=football user=postgres password=qwas') or die('Error: ' . pg_errormessage($conn));
-	var_dump($conn);
-	echo 'Yes! Connected!';
-	$query = 'SELECT id, name FROM groups';
-	$query = 'SELECT groups.name, commands.id, commands.country 
-FROM groups 
-JOIN commands
-on groups.id=commands.id_group';
+	/*	var_dump($conn);
+		echo 'Yes! Connected!';
+		$query = 'SELECT id, name FROM groups';
+		$query = 'SELECT groups.name, commands.id, commands.country
+	FROM groups
+	JOIN commands
+	on groups.id=commands.id_group';
 
-	$query = 'SELECT * 
-	FROM groups, commands
-WHERE groups.id= commands.id_group';
-	var_dump($query);
+		$query = 'SELECT *
+		FROM groups, commands
+	WHERE groups.id= commands.id_group';
+		var_dump($query);
+		$result = pg_query($conn, $query);
+		$row = pg_fetch_all($result);
+		echo '<pre>';
+		var_dump($row);
+		echo '</pre>';
+		echo 'группа2: ' . $row[1]['name'];*/
+
+
+	$query = 'SELECT id, name FROM groups';
 	$result = pg_query($conn, $query);
-	$row = pg_fetch_all($result);
-	echo '<pre>';
-	var_dump($row);
-	echo '</pre>';
-	echo 'группа2: ' . $row[1]['name'];
+
+	while ($row = pg_fetch_assoc($result)) {
+		echo '<h3>Группа ' . $row['name'] . '</h3>';
+		$row_com = pg_fetch_all(select_com($row['id'], $conn));
+		if ($row_com != null) {
+			foreach ($row_com as $rc) {
+				echo $rc['id'] . ' ' . $rc['country'] . '<br>';
+			}
+		}
+	}
+	pg_close($conn);
+	function select_com($numgroup, $conn)
+	{
+		$query = 'SELECT * from commandview WHERE id_group=' . $numgroup;
+		$result = pg_query($conn, $query);
+		return $result;
+	}
